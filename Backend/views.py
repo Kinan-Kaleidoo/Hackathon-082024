ebfe003c1112d61a838a1239746a39bd5eb006c4
dordavid <111dordavid@gmail.com>
2024-08-05 17:43:59 +0300

    back-dor

--- Backend/views.py
+++ Backend/views.py
@@ -2,17 +2,11 @@ import os
 from flask import render_template, request, redirect, session, url_for, jsonify
 import requests
 from db import  find_user_by_username, users_collection
-import pandas as pd
-import yfinance as yf
-import datetime as dt
-import numpy as np
-import pandas as pd
-from matplotlib import style
-from statsmodels.tsa.seasonal import STL
-from sklearn.metrics import mean_squared_error, accuracy_score
-from iexfinance.stocks import Stock
+
 from flask_login import login_required
 from db import find_user_by_username, update_prefernces, buy_share1, get_user_purchases
+import magic
+
 
 
 from flask_login import login_required
@@ -25,28 +19,6 @@ df = _df.copy()
 def login():
     return render_template('login.html')
 
-
-@login_required
-def company_chart():
-    company_name = request.args.get('company')
-    share = request.args.get('share')  
-    actual_date = dt.date.today()
-    past_date = actual_date - dt.timedelta(days=365 * 5)
-    actual_date = actual_date.strftime("%Y-%m-%d")
-    past_date = past_date.strftime("%Y-%m-%d")
-    data = yf.download(share, start=past_date, end=actual_date)
-    df = pd.DataFrame(data).reset_index()
-    # company_data = df[df['Company'] == company_name]
-    def format_data(company_data):
-        return {
-            'dates': company_data['Date'].tolist(),
-            'closes': company_data['Close'].tolist(),
-            'opens': company_data['Open'].tolist(),
-            'highs': company_data['High'].tolist(),
-            'lows': company_data['Low'].tolist()
-        }
-    data = format_data(df)
-    return render_template('chart.html', company=company_name, share=share,  data1=data)
 
 @login_required
 def index():
@@ -63,7 +35,24 @@ def search():
 
 @login_required
 def doc():
-    return render_template('index.html')
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+        if file:
+            file_content = file.read()
+            mime = magic.Magic(mime=True)
+            file_type = mime.from_buffer(file_content)
+            if file_type in ["application/pdf", "text/plain", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]:
+                return jsonify({"filename": file.filename, "format": file_type}), 200
+            else:
+                return jsonify({"error": "File is not a PDF, TXT, or WORD document", "file_type": file_type}), 400
+        return jsonify({"error": "No file provided"}), 400
+    else:
+        return jsonify("good"), 200
+
 
 @login_required
 def audio():
