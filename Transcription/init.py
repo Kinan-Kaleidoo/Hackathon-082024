25f02cd20611f26945b7e50ee136325c91466343
Haim Goldfisher <haimngoldfisher@gmail.com>
2024-08-05 16:38:07 +0300

    first commit

---
+++ Transcription/init.py
@@ -0,0 +1,10 @@
+import torch
+from faster_whisper import WhisperModel
+
+# Initialize the Whisper model
+def load_model():
+    if torch.cuda.is_available():
+        model = WhisperModel("ivrit-ai/faster-whisper-v2-d3-e3")  # GPU case
+    else:
+        model = WhisperModel('base')  # CPU case
+    return model
---
+++ Transcription/utils.py
@@ -0,0 +1,21 @@
+import subprocess
+
+# Allowed extensions
+ALLOWED_EXTENSIONS = {'mp3', 'wav', 'mp4', 'opus'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Function to extract audio from video
+def extract_audio_from_video(video_path: str, audio_output_path: str) -> None:
+    command = [
+        'ffmpeg',
+        '-y',  # Overwrite output files without asking
+        '-i', video_path,
+        '-vn',  # Disable video
+        '-acodec', 'pcm_s16le',  # Audio codec
+        '-ar', '44100',  # Sampling rate
+        '-ac', '2',  # Number of channels
+        audio_output_path
+    ]
+    subprocess.run(command, check=True)
